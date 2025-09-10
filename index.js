4ff40200edb712e316932bed1de7df6ed7d0bf3e
farzad-d <fzddnp@hotmail.com>
2025-09-10 13:24:53 +0330

    Add styles and adjust card layouts

--- index.js
+++ index.js
@@ -17,10 +17,21 @@ function addBookToLibrary(title, author, pages, readState) {
   myLibrary.push(book);
 }
 
+addBookToLibrary("War and Peace", "Leo Tolstoy", 1200, false);
 addBookToLibrary("Animal Farm", "George Orwell", 110, true);
+addBookToLibrary("Don Quixote", "Miguel de Cervantes", 998, false);
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 352, true);
 addBookToLibrary("Wuthering Heights", "Emily Brontë", 252, true);
 addBookToLibrary("Little Women", "Louisa May Alcott", 282, false);
 // console.log(myLibrary);
+
+function truncText(str) {
+  if (str.length > 20) {
+    return str.slice(0, 20) + "...";
+  } else {
+    return str;
+  }
+}
 
 const cards = document.querySelector("#cards");
 
@@ -28,26 +39,31 @@ function cardCreator(bookTitle, bookAuthor, bookPages, bookState, bookId) {
   const card = document.createElement("section");
   card.dataset.id = bookId;
   card.classList.add("card");
+  if (bookState) card.classList.add("readBgColor");
 
   const title = document.createElement("h3");
-  title.textContent = `Title: ${bookTitle}`;
+  title.textContent = `Title: ${truncText(bookTitle)}`;
   card.appendChild(title);
 
   const author = document.createElement("p");
-  author.textContent = `Author: ${bookAuthor}`;
+  author.textContent = `Author: ${truncText(bookAuthor)}`;
   card.appendChild(author);
 
   const pages = document.createElement("p");
-  pages.textContent = `Pages: ${bookPages}`;
+  pages.textContent = `Pages: ${truncText(bookPages)}`;
   card.appendChild(pages);
 
   const state = document.createElement("p");
   state.textContent = `Read: ${bookState ? "✅" : "❌"}`;
   card.appendChild(state);
 
+  const cardBtns = document.createElement("div");
+  cardBtns.classList.add("card-btns");
+  card.appendChild(cardBtns);
+
   const delBtn = document.createElement("button");
-  delBtn.textContent = "Del";
-  card.appendChild(delBtn);
+  delBtn.textContent = "Delete";
+  cardBtns.appendChild(delBtn);
   delBtn.addEventListener("click", () => {
     myLibrary = myLibrary.filter((book) => card.dataset.id !== book.id);
     displayLibrary(myLibrary);
@@ -55,7 +71,7 @@ function cardCreator(bookTitle, bookAuthor, bookPages, bookState, bookId) {
 
   const sttBtn = document.createElement("button");
   sttBtn.textContent = bookState ? "Mark as Unread" : "Mark as Read";
-  card.appendChild(sttBtn);
+  cardBtns.appendChild(sttBtn);
   sttBtn.addEventListener("click", () => {
     myLibrary.find((book) => {
       if (card.dataset.id === book.id) book.changeStt();
@@ -78,7 +94,7 @@ displayLibrary(myLibrary);
 
 const addBookBtn = document.querySelector("#add-book");
 const dialog = document.querySelector("dialog");
-const closeDialogBtn = document.querySelector("dialog > button");
+const closeDialogBtn = document.querySelector(".close-btn");
 
 addBookBtn.addEventListener("click", () => {
   dialog.showModal();
@@ -86,9 +102,7 @@ addBookBtn.addEventListener("click", () => {
 });
 closeDialogBtn.addEventListener("click", () => dialog.close());
 
-const submitBtn = document.querySelector('[type="submit"]');
 const form = document.querySelector("form");
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
